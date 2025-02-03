830f05deeab20b943c890ab5d365b044633374ff
TiraMutiar <Tiramutiar4@gmail.com>
2025-02-03 12:55:14 +0700

    masukin template

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function index(){
-        return view('lala');
+        return view('layout.main');
+    }
+    public function dash_mas(){
+        return view('masyarakat.dash');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
 Route::get('/', [LoginController::class, 'index']);
+Route::get('/dash-mas', [LoginController::class, 'dash_mas']);
+
